89a6c408c9ca0f87df064f748763c767fba68516
AdindaFebyola <adinfebyola@gmail.com>
2025-06-08 20:15:11 +0700

    feat: Implement navigation from options to login and register screens

--- app/src/main/java/com/example/greenfresh/OpsiActivity.java
+++ app/src/main/java/com/example/greenfresh/OpsiActivity.java
@@ -1,13 +1,40 @@
 package com.example.greenfresh;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OpsiActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private TextView textRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opsi);
+
+        buttonLogin = findViewById(R.id.button_login);
+        textRegister = findViewById(R.id.text_register);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OpsiActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OpsiActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
