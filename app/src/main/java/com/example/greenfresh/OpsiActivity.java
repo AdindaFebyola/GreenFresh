4542e8ae1db0e973d3a727040fbf94a62006b71c
AdindaFebyola <adinfebyola@gmail.com>
2025-06-08 19:50:18 +0700

    feat: Initial project setup and Opsi Screen

---
+++ app/src/main/java/com/example/greenfresh/OpsiActivity.java
@@ -0,0 +1,13 @@
+package com.example.greenfresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpsiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opsi);
+    }
+}
